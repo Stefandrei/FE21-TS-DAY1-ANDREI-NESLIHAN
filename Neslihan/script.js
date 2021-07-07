76d33001b62f78c31b32dea06d7a680133331551
Neslihan Minaz <minaz_nesli@hotmail.com>
2021-07-07 10:47:06 +0200

    Basic 1

---
+++ Neslihan/script.js
@@ -0,0 +1,16 @@
+// First basic exercise
+var max_mul = 10;
+var arr_one_to_ten = [];
+for (var i = 1; i <= max_mul; i++) {
+    arr_one_to_ten.push(i);
+}
+document.getElementById("head").innerHTML = "Multiply numbers from 1 to " + max_mul.toString();
+var mul_tbl = document.getElementById("multiply");
+for (var _i = 0, arr_one_to_ten_1 = arr_one_to_ten; _i < arr_one_to_ten_1.length; _i++) {
+    var val = arr_one_to_ten_1[_i];
+    mul_tbl.innerHTML += "<h3>by" + val.toString();
+    for (var _a = 0, arr_one_to_ten_2 = arr_one_to_ten; _a < arr_one_to_ten_2.length; _a++) {
+        var val_1 = arr_one_to_ten_2[_a];
+        mul_tbl.innerHTML += val_1.toString() + " X " + val_1.toString() + " = " + val_1 * val_1 + "<br>";
+    }
+}
